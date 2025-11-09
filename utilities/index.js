9b01ddc07196fba1d541ecbb284372f7c0c3705c
FaithIdowu123 <ofaith@byupathway.edu>
2025-11-09 23:46:31 +0100

    week3

--- utilities/index.js
+++ utilities/index.js
@@ -59,3 +59,10 @@ Util.buildClassificationGrid = async function(data){
 }
 
 module.exports = Util
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
